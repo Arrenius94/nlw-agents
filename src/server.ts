b5ab64346e0e6d2b89071ff9f17f9baec6a8b39d
Arrenius94 <arrenius94@gmail.com>
2025-07-16 22:15:13 -0300

    finish back end

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
 import { fastifyCors } from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
 import { env } from "./env.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoomRoute } from "./http/routes/create-room.ts";
 import { getRoomsQuestions } from "./http/routes/get-rooms-questions.ts";
 import { createQuestionRoute } from "./http/routes/create-questions.ts";
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -13,6 +15,7 @@ app.register(fastifyCors, {
   origin: "http://localhost:5173",
 });
 
+app.register(fastifyMultipart);
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
@@ -24,6 +27,7 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomsQuestions);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Port: ${process.env.PORT}`);
